31e8cf89ad60242a2255e782c99f4c97fdf933b6
CSavi <carrielane89@gmail.com>
2018-08-16 13:10:19 -0400

    added routes in config.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,16 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  root to: 'welcome#index'
+
+  resources :instructors
+  resources :students
+  resources :assignments
+  resources :lessons
+
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+
+  delete '/logout' => 'sessions#delete'
+
 end
